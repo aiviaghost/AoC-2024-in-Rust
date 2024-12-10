ba33aad9c3e6d922ee4d8f3cfe5655bdfd44fa27
aiviaghost <smurfplatta@gmail.com>
2024-12-10 22:07:38 +0100

    Add neighbour arrays to template

--- template.rs
+++ template.rs
@@ -1,5 +1,17 @@
 use std::fs;
 
+const NEXTS4: [(isize, isize); 4] = [(0, 1), (0, -1), (1, 0), (-1, 0)];
+const NEXTS8: [(isize, isize); 8] = [
+    (-1, -1),
+    (0, -1),
+    (1, -1),
+    (1, 0),
+    (1, 1),
+    (0, 1),
+    (-1, 1),
+    (-1, 0),
+];
+
 fn solve_1(input: Vec<String>) {}
 
 fn solve_2(input: Vec<String>) {}
